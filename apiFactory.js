5c5731df0e4fead55bfd16114e7f194fe6c13292
Mike Yates <mike@yatesdev.com>
2020-02-22 18:12:43 -0500

    Conversion to a factory based approach

---
+++ apiFactory.js
@@ -0,0 +1,17 @@
+const azdev = require('azure-devops-node-api');
+
+const apiFactory = async(config) => {
+  const authHandler = azdev.getPersonalAccessTokenHandler(config.AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN);
+  const connection = new azdev.WebApi(config.ORG_URL, authHandler);
+  const projectApi = await connection.getCoreApi();
+  const gitApi = await connection.getGitApi();
+
+  return {
+    projectApi,
+    gitApi
+  };
+}
+
+module.exports = {
+  apiFactory
+}
--- index.js
+++ index.js
@@ -1,62 +1,13 @@
-const azdev = require('azure-devops-node-api');
 const inquirer = require('inquirer');
 const program = require('commander');
 const dotenv = require('dotenv');
 const open = require('open');
 const fs = require('fs');
 const path = require('path');
+const { apiFactory } = require('./apiFactory');
+const PromptFactory = require('./prompts');
 
 const configFilePath = path.join( __dirname,'./.env');
-
-const projectSelectorQuestionFactory = (projects) => [{
-  type: 'list',
-  name: 'project',
-  message: 'Which project?',
-  choices() {
-    return projects.sort((a, b) => (a.name > b.name) ? 1 : -1) // sort alphabetically
-      .map((project) => ({
-        name: project.name,
-        value: [project],
-        short: project.name
-      })
-    );
-  }
-}];
-
-const prSelectorQuestionFactory = (pullRequests) => [{
-  type: 'list',
-  name: 'pullRequest',
-  message: 'Which PR to view?',
-  choices() {
-    return pullRequests.map((pullRequest) => ({
-      name: program.all ? `${pullRequest.repository.project.name}: ${pullRequest.title}` : pullRequest.title,
-      value: pullRequest,
-      short: pullRequest.title
-    }));
-  }
-}];
-
-const envFirstRunQuestionFactory = () => [
-  {
-    type: 'input',
-    name: 'orgUrl',
-    message: 'Azure DevOps instance URL:',
-    validate: function(value) {
-      var pass = value.match(
-        /^(http(s)?:\/\/)[\w.-]+(?:\.[\w]+)+$/i
-      );
-      if (pass) {
-        return true;
-      }
-
-      return 'Please enter a valid url (no trailing slash)';
-    }
-  },{
-    type: 'input',
-    name: 'token',
-    message: 'Personal Access Token:'
-  }
-];
 
 const resetConfigHandler = () => {
   if (fs.existsSync(configFilePath)) {
@@ -69,25 +20,13 @@ const resetConfigHandler = () => {
 
 const configBuilder = async() => {
   if(!fs.existsSync(configFilePath)) {
-    const config = await inquirer.prompt(envFirstRunQuestionFactory());
+    const config = await inquirer.prompt(firstRunPrompt());
     fs.writeFileSync(configFilePath,
     `ORG_URL=${config.orgUrl}
      AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN=${config.token}`);
   }
   return dotenv.config({ path: configFilePath }).parsed;
 };
-
-const apiFactory = async(config) => {
-  const authHandler = azdev.getPersonalAccessTokenHandler(config.AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN);
-  const connection = new azdev.WebApi(config.ORG_URL, authHandler);
-  const projectApi = await connection.getCoreApi();
-  const gitApi = await connection.getGitApi();
-
-  return {
-    projectApi,
-    gitApi
-  };
-}
 
 const main = async() => {
   program
@@ -102,6 +41,12 @@ const main = async() => {
   }
 
   const config = await configBuilder();
+  const {
+    projectPrompt,
+    prSelectorPrompt,
+    firstRunPrompt
+  } = PromptFactory(config);
+  console.log(prSelectorPrompt);
   const { projectApi, gitApi } = await apiFactory(config);
 
   const projects = await projectApi.getProjects();
@@ -113,7 +58,7 @@ const main = async() => {
       project: projects
     };
   } else {
-    projectSelection = await inquirer.prompt(projectSelectorQuestionFactory(projects));
+    projectSelection = await inquirer.prompt(projectPrompt(projects));
   }
 
   const prByProjectRequests = projectSelection.project.map((project) => 
@@ -121,7 +66,7 @@ const main = async() => {
 
   const pullRequests = await Promise.all(prByProjectRequests)
     .then((prsByProject) => {
-        return [].concat(...prsByProject);
+      return [].concat(...prsByProject);
     });
 
   if (pullRequests.length == 0) {
@@ -129,7 +74,7 @@ const main = async() => {
     return;
   }
 
-  const prAnswer = await inquirer.prompt(prSelectorQuestionFactory(pullRequests));
+  const prAnswer = await inquirer.prompt(prSelectorPrompt(pullRequests, { showAll: program.all }));
 
   const prUrl = `${config.ORG_URL}/${prAnswer.pullRequest.repository.project.id}/_git/${prAnswer.pullRequest.repository.id}/pullrequest/${prAnswer.pullRequest.pullRequestId}`;
   await open(prUrl);
